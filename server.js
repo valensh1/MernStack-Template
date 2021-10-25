500ed772eda05e5301eb2e417653bd820738c692
Shaun Valentine <valensh1@yahoo.com>
2021-10-25 16:06:37 -0700

    deployment changes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', async (req, res) => {
 });
 
 //? DEPLOYMENT
-const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
