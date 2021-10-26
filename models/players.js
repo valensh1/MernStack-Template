65a07ff748d380a323f2d8472afdc146179334b6
Shaun Valentine <valensh1@yahoo.com>
2021-10-25 23:30:44 -0700

    Notes added to server.js and models file (players.js)

--- models/players.js
+++ models/players.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-//? SETTING UP mongoDB SCHEMA
+//? SETTING UP MongoDB SCHEMA
 const playerSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -19,7 +19,8 @@ const playerSchema = new mongoose.Schema({
 //? TELLING MONGOOSE YOU WANT TO CREATE A MODEL USING OUR SCHEMA
 const Player = mongoose.model('Player', playerSchema); // Player is our model name and it MUST BE SINGULAR WITH AN UPPERCASE FIRST LETTER! mongoDB will then lowercase this model name and make it plural so it will change the name to ---> players
 
-// const player1 = Player.create({ // Creation of a player and saved to database. Want to comment this out otherwise it will keep saving data to mongoDB and you will have duplicates
+//? CREATION OF FIRST SET OF DATA FOR DATABASE
+// const player1 = Player.create({ // Creation of a player1 and saved to database. Want to comment this out otherwise it will keep saving data to mongoDB each time you save this file and you will have duplicates in your MongoDB
 //   firstName: 'Shaun',
 //   lastName: 'Valentine',
 //   number: 23,
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-require('dotenv').config();
+//? REQUIREMENTS - npm Packages
+require('dotenv').config(); // Requirement to be able to use .env file so we can reference passwords without displaying them in code.
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const path = require('path'); // Provides utilities for working with file and directory paths
+const PORT = process.env.PORT || 5000; // Creation of variable that is either the port the hosting service runs our application on when deployed or localhost:5000 if working on our local machine
+const app = express(); // Creation of variable app which calls the express function
+app.use(cors()); // Every time file is saved the application runs the cors function
 
-const app = express();
-app.use(cors());
-
-const Player = require('./models/players.js');
-const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI;
-const db = mongoose.connection;
+//? MONGOOSE/MongoDB SET-UP
+const Player = require('./models/players.js'); // Creation of variable to pass our Player Model (or whatever you called your model) to this file so we can use our Model in this file when accessing Mongoose/MongoDB
+const mongoose = require('mongoose'); // Need to require Mongoose in order to access our MongoDB database; Mongoose is an Object Data Modeling (ODM) library for MongoDB and Node.js. It manages relationships between data, provides schema validation, and is used to translate between objects in code and the representation of those objects in MongoDB.
+const MONGODB_URI = process.env.MONGODB_URI; // Creation of variable which stores our MongoDB connection string containing our password to access our database. This connection string is stored on our .env file
+const db = mongoose.connection; // Creation of variable to shorten our code. Instead of having to type mongoose.connection each time we can just refer to it as db.
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -24,6 +25,7 @@ db.on('open', () => {
 app.use(express.urlencoded({ extended: true })); // Middleware - so we can use req.body otherwise Express can't read what's in the body of our POST request. This Middleware parses data from forms in the x-www-form-urlencoded format
 app.use(express.json()); // Middleware - for parsing data coming in the body of the POST request as json format
 
+//? API Routes
 app.get('/players', async (req, res) => {
   try {
     const rosters = await Player.find({});
@@ -41,10 +43,12 @@ app.get('/', async (req, res) => {
   }
 });
 
-//? DEPLOYMENT
-
+//? DEPLOYMENT CODE
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  // When .env file has NODE_ENV=production in it run this code below (we must put this in our .env file for when deploying)
+  app.use(express.static(path.join(__dirname, '/client/build'))); // When .env file has NODE_ENV=production then look for the static file in the /client/build folder. This folder won't be there until you go into the client folder and run npm run build command in Terminal.
+
+  // Any routes not shown above in API routes this code will send a file from the /client/build/index.html file which is basically our React front-end files
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
@@ -55,5 +59,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(PORT, () => {
+  // Code to notify you that app is running on PORT 5000 on local computer.
   console.log(`Listening on PORT ${PORT} yo`);
 });
